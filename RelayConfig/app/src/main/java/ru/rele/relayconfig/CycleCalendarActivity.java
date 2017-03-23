ad7f1256084756915d045cabc06a28b58cfb1064
Artem Abramov <tematibr@gmail.com>
2017-03-23 11:45:24 +0300

    Finish onCreate method for CycleCalendarActivity.
    Modified code and layout files. Left only 2 out of 5 radio
    buttons to simplify code.

--- RelayConfig/app/src/main/java/ru/rele/relayconfig/CycleCalendarActivity.java
+++ RelayConfig/app/src/main/java/ru/rele/relayconfig/CycleCalendarActivity.java
@@ -2,9 +2,18 @@ package ru.rele.relayconfig;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
 
 public class CycleCalendarActivity extends AppCompatActivity {
+
+    enum DAYS_SELECT_MODE {
+        SINGLE_DAY
+        , WORKING_DAYS
+    }
+
+    DAYS_SELECT_MODE currentMode = DAYS_SELECT_MODE.SINGLE_DAY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +27,30 @@ public class CycleCalendarActivity extends AppCompatActivity {
         displayLayout.removeAllViews();
         displayLayout.addView(currentCycle);
 
-        LinearLayout calendarLayout = (LinearLayout) findViewById(R.id.yearCalendar);
-        displayLayout.removeAllViews();
-        displayLayout.addView(currentCycle);
+        // Control radio buttons to select the mode
+        final RadioButton everyDay = (RadioButton) findViewById(R.id.buttonEveryDay);
+        final RadioButton workingDays = (RadioButton) findViewById(R.id.buttonWorkingDays);
+        everyDay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // turn off other radio buttons
+                workingDays.setChecked(false);
+                currentMode = DAYS_SELECT_MODE.SINGLE_DAY;
+            }
+        });
+        workingDays.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // turn off other radio buttons
+                everyDay.setChecked(false);
+                currentMode = DAYS_SELECT_MODE.WORKING_DAYS;
+            }
+        });
 
+        // Put the calendar into the layout.
+        LinearLayout calendarLayout = (LinearLayout) findViewById(R.id.yearCalendar);
+        calendarLayout.removeAllViews();
+        calendarLayout.addView(((MainApplication) getApplication()).getCalendarControl());
 
     }
 }
