6cb3327cf23b04b271596267417a08e36474b3c1
Artem Abramov <tematibr@gmail.com>
2017-03-24 18:17:15 +0300

    Work on passing data between activities.
    One control can not belong to multiple layouts,
    remoed code from MainApplication that was used
    to pass Controls around. Can only pass data.

--- RelayConfig/app/src/main/java/ru/rele/relayconfig/MainApplication.java
+++ RelayConfig/app/src/main/java/ru/rele/relayconfig/MainApplication.java
@@ -11,32 +11,8 @@ import com.github.ik024.calendar_lib.custom.YearView;
 
 public class MainApplication extends Application {
 
-    private CycleControl currentCycle;
-    public void setCurrentCycle(CycleControl cc) {
-        currentCycle = cc;
-    }
-
-    // You should not need to ever reset CurrentCycle
-    // to null because the user might "go back" to previous
-    // screen and the CurrentCycle should still be active
-    public CycleControl getCurrentCycle() {
-        if (currentCycle == null) throw new AssertionError("CurrentCycle has not been set OR has been reset to null");
-        return currentCycle;
-    }
-
     private RelayCalendarData calendar = new RelayCalendarData();
     public RelayCalendarData getCalendar() {
         return calendar;
-    }
-
-    private YearView calendarControl;
-    public YearView getCalendarControl() {
-        return calendarControl;
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        calendarControl = new YearView(getBaseContext());
     }
 }
